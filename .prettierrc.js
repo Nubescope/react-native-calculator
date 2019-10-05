a9eb95be109a731f337379188277c86af7e85156
Juan Pablo Garcia <juanpablogarcia@gmail.com>
2019-10-05 19:16:20 -0300

    Configure prettier / eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  printWidth: 120,
+  tabWidth: 2,
   singleQuote: true,
-  trailingComma: 'all',
-};
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  semi: false,
+  useTabs: false,
+  jsxBracketSameLine: false,
+}
